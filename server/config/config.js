0496ca021233e9f1a2a4bf14fd01401dcab56353
Jair Tello <jairdeyner730791@gmail.com>
2021-01-29 17:29:59 -0500

    Login

--- server/config/config.js
+++ server/config/config.js
@@ -5,6 +5,12 @@
 /**ENTORNO */
 process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
+/**VENCIMIENTO DEL TOKEN */
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+/**SEED DE AUTENTICACION */
+process.env.SEED = process.env.SEED || "este-es-el-seed-desarrollo";
+
 /**BASE DE DATOS */
 let urlDB;
 
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,10 +3,14 @@ const bcrypt = require("bcrypt");
 const _ = require("underscore");
 
 const Usuario = require("../models/usuario");
+const {
+  verificaToken,
+  verificaAdmin_Role,
+} = require("../middlewares/autenticacion");
 
 const app = express();
 
-app.get("/usuario", function (req, res) {
+app.get("/usuario", verificaToken, (req, res) => {
   let desde = req.query.desde || 0;
   desde = Number(desde);
 
@@ -34,7 +38,7 @@ app.get("/usuario", function (req, res) {
     });
 });
 
-app.post("/usuario", function (req, res) {
+app.post("/usuario", [verificaToken, verificaAdmin_Role], (req, res) => {
   let body = req.body;
 
   let usuario = new Usuario({
@@ -59,7 +63,7 @@ app.post("/usuario", function (req, res) {
   });
 });
 
-app.put("/usuario/:id", function (req, res) {
+app.put("/usuario/:id", [verificaToken, verificaAdmin_Role], (req, res) => {
   let id = req.params.id;
   let body = _.pick(req.body, ["nombre", "email", "role", "estado"]);
 
@@ -83,7 +87,7 @@ app.put("/usuario/:id", function (req, res) {
   );
 });
 
-app.delete("/usuario/:id", function (req, res) {
+app.delete("/usuario/:id", [verificaToken, verificaAdmin_Role], (req, res) => {
   let id = req.params.id;
 
   let cambiaEstado = {

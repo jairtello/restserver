e5fb8a4830917f57fb6198f112ca06cbb08566af
Jair Tello <jairdeyner730791@gmail.com>
2021-01-28 19:23:05 -0500

    Rest Server - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,4 +2,19 @@
  * PUERTO
  */
 
+/**ENTORNO */
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+
+/**BASE DE DATOS */
+let urlDB;
+
+if (process.env.NODE_ENV === "dev") {
+  urlDB = "mongodb://localhost:27017/market";
+} else {
+  urlDB =
+    "mongodb+srv://jair:4fzPhb4fjRvhAzDa@cluster0.wiifn.mongodb.net/market";
+}
+
+process.env.URLDB = urlDB;
+
 process.env.PORT = process.env.PORT || 3000;
